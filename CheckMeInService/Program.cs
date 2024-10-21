6af83831fc1d0655e273cc4e2b919e93e2eaabd5
TechGlue <garcialuis42@outlook.com>
2024-10-21 15:20:18 -0700

    misc.

--- CheckMeInService/Program.cs
+++ CheckMeInService/Program.cs
@@ -2,7 +2,6 @@ using CheckMeInService.CheckIns;
 using CheckMeInService.Data;
 using CheckMeInService.Models;
 using CheckMeInService.Subscribers;
-using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 DatabaseSettings settings = new DatabaseSettings("appsettings.json");
@@ -12,11 +11,10 @@ builder.Services.AddSingleton<SubscriptionQueries>();
 builder.Services.AddSingleton<CheckInQueries>();
 var app = builder.Build();
 
-// Enable this later once the Azure SQL DB is configured
-// if (settings.TestConnection() is false)
-// {
-//     throw new Exception("Unable to connect to the database");
-// }
+if (settings.TestConnection() is false)
+{
+    throw new Exception("Unable to connect to the database");
+}
 
 app.MapGet("/", () => "CheckMeIn API is running");
 
